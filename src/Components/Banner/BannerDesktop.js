4eb6b536d43c944d851edbf222969f80d8614d7c
viren1610 <virenplays@gmail.com>
2022-12-28 20:46:45 +0530

    App Banner

--- src/Components/Banner/BannerDesktop.js
+++ src/Components/Banner/BannerDesktop.js
@@ -1,11 +1,18 @@
-import { BannerContainer } from "../../Styles/Banner";
+import { Typography } from "@mui/material";
+import { BannerContainer, BannerContent, BannerDescription, BannerTitle } from "../../Styles/Banner";
 
 
 export default function BannerDesktop(matches){
 return(
 
 <BannerContainer>
-hey
+<BannerContent>
+    <Typography variant="h6">Huge Collection</Typography>
+    <BannerTitle>SALEEEEEEE</BannerTitle>
+    <BannerDescription variant="subtitle">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    </BannerDescription>
+</BannerContent>
 </BannerContainer>
 
 );
--- src/Styles/Banner/index.js
+++ src/Styles/Banner/index.js
@@ -1,14 +1,52 @@
 import { Box, height } from "@mui/system";
 import { styled } from "@mui/material/styles";
 import { Colors } from "../theme";
+import { Typography } from "@mui/material";
 
-export const BannerContainer=styled(Box)(()=>({
+export const BannerContainer=styled(Box)(({theme})=>({
     display:'flex',
     alignItems:'center',
     justifyContent:'center',
     width:"100%",
     height:"100%",
     padding:"0px 0px",
-    background: Colors.light_gray
+    background: Colors.light_gray,
+    [theme.breakpoints.down('sm')] :  {
+        flexDirection:'initial',
+        alignItems:'center',
+    }
 
 }));
+
+export const BannerContent=styled(Box)(({theme})=>({
+    display:'flex',
+    alignItems:"center",
+    justifyContent:'center',
+    padding:'30px',
+    flexDirection:'column',
+    [theme.breakpoints.down('sm')]:{
+        flexDirection:''
+    }   
+}))
+
+
+export const BannerTitle =styled(Typography)(({theme})=>({
+       lineHeight:'1.5',
+       fontSize:'72px',
+       marginBottom:'20px',
+       [theme.breakpoints.down('sm')]:{
+        fontSize:'42px',
+       }
+}))
+
+export const BannerDescription =styled(Typography)(({theme})=>({
+    lineHeight:'1.25',
+    marginBottom:'3em',
+    letterSpacing:'1.25',
+    [theme.breakpoints.down('sm')]:{
+        lineHeight:'1.15',
+        marginBottom:'1.5em',
+        letterSpacing:'1.15',
+    }
+}))
+
